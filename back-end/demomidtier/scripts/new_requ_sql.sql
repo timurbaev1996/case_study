1d86616b0fd41809a986ae75b61e84c132a06748
andreastichy91 <andreas.tichy@icloud.com>
2018-08-10 14:24:27 +0200

    Update querries partII

--- back-end/demomidtier/scripts/new_requ_sql.sql
+++ back-end/demomidtier/scripts/new_requ_sql.sql
@@ -6,14 +6,16 @@ SELECT * FROM instrument;
 /* USER REQUIREMENT 4 */
 
 /* Correlation btw. deal_type and counterparty --> SHows the most frequently traded Counterparty */
-SELECT d.deal_type, d.deal_quantity, d.deal_amount, c.counterparty_name
+SELECT sum(d.deal_quantity), sum(d.deal_amount), c.counterparty_name
 	FROM deal d, counterparty c
-    WHERE d.deal_counterparty_id = c.counterparty_id;
+    WHERE d.deal_counterparty_id = c.counterparty_id
+    group by c.counterparty_name;
     
 /* Correlation btw. deal_amount, deal_quantity and instrument --> Shows which instruments traded the most with the highest amount */
-SELECT d.deal_type, d.deal_quantity, d.deal_amount, i.instrument_name
-	FROM deal d, instrumnet i
-    WHERE d.deal_instrument_id = i.instrument_id;
+SELECT sum(d.deal_quantity), sum(d.deal_amount), i.instrument_name
+	FROM deal d, instrument i
+    WHERE d.deal_instrument_id = i.instrument_id
+    group by i.instrument_name;
 
 
 /* NEW REQUIREMENTS */
@@ -21,13 +23,13 @@ SELECT d.deal_type, d.deal_quantity, d.deal_amount, i.instrument_name
 /* Requirement 1 
 What are the average buy and sell prices for each instrument during the period? - importance (3), accuracy (4)
 */
-SELECT AVG(d.deal_amount) AS avg_sell_price, i.instrument_name
+SELECT i.instrument_name, AVG(d.deal_amount) AS avg_sell_price
   FROM deal d, instrument i
   WHERE d.deal_type = 'S' 
   AND d.deal_instrument_id = i.instrument_id
   Group by i.instrument_name;
 
-SELECT AVG(d.deal_amount) AS avg_sell_price, i.instrument_name
+SELECT i.instrument_name, AVG(d.deal_amount) AS avg_sell_price
   FROM deal d, instrument i
   WHERE d.deal_type = 'B' 
   AND d.deal_instrument_id = i.instrument_id
@@ -37,46 +39,47 @@ SELECT AVG(d.deal_amount) AS avg_sell_price, i.instrument_name
 What are the dealers ending positions (i.e. how many net trades have they done on each instrument)? - importance (4), accuracy (5)
 */
 
-SELECT SUM(d.deal_quantity) AS end_position_sell, i.instrument_name
-  FROM deal d, instrument i
-  WHERE d.deal_type = 'S' 
-  AND d.deal_instrument_id = i.instrument_id
-  Group by i.instrument_name; 
-  
-  
-SELECT SUM(d.deal_quantity) AS end_position_buy, i.instrument_name
-  FROM deal d, instrument i
-  WHERE d.deal_type = 'B' 
-  AND d.deal_instrument_id = i.instrument_id
-  Group by i.instrument_name;
-
-  
-/* end_position_sell - end_position_buy
+select  c.counterparty_name, i.instrument_name, SUM(case when d.deal_type = 'S' then d.deal_quantity else 0 end) - SUM(case when d.deal_type = 'B' then d.deal_quantity else 0 end) as net_trade
+FROM  deal d
+INNER JOIN counterparty c on d.deal_counterparty_id = c.counterparty_id 
+INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
+  Group by i.instrument_name, c.counterparty_name;
   
 
 /* Requirement 3
 What is the realised profit/loss for each dealer (i.e. net sum of their trade values)? - importance (3), accuracy (5)
 */
 
-  
-SELECT SUM(d.deal_amount) AS sum_sell, c.counterparty_name
-  FROM deal d, counterparty c
-  WHERE d.deal_type = 'S' 
-  AND d.deal_counterparty_id = c.counterparty_id
-  GROUP BY c.counterparty_name;
+select  c.counterparty_name, i.instrument_name, 
+(SUM(case when d.deal_type = 'S' then d.deal_amount * d.deal_quantity else 0 end))
+- 
+(SUM(case when d.deal_type = 'B' then d.deal_amount *d.deal_quantity else 0 end) )
+as effective_result
+FROM  deal d
+INNER JOIN counterparty c on d.deal_counterparty_id = c.counterparty_id 
+INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
+  Group by i.instrument_name, c.counterparty_name;  
+/*
+select  c.counterparty_name, i.instrument_name, SUM(case when d.deal_type = 'S' then d.deal_amount else 0 end) - SUM(case when d.deal_type = 'B' then d.deal_amount else 0 end) as net_sum
+FROM  deal d
+INNER JOIN counterparty c on d.deal_counterparty_id = c.counterparty_id 
+INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
+  Group by i.instrument_name, c.counterparty_name;  
+*/
 
-SELECT SUM(d.deal_amount) AS sum_buy, c.counterparty_name
-  FROM deal d, counterparty c
-  WHERE d.deal_type = 'b' 
-  AND d.deal_counterparty_id = c.counterparty_id
-  GROUP BY c.counterparty_name;
-  
-/* First Query - Second Query */
 
 /* Requirement 4
 What is the effective profit/loss for each dealer (i.e. resolving their positions using the ending buy/sell price)? - importance (4), accuracy (5)
 */
 
-
+select  c.counterparty_name, i.instrument_name, 
+(SUM(case when d.deal_type = 'S' then d.deal_amount * d.deal_quantity else 0 end))
+- 
+(SUM(case when d.deal_type = 'B' then d.deal_amount *d.deal_quantity else 0 end) )
+as effective_result
+FROM  deal d
+INNER JOIN counterparty c on d.deal_counterparty_id = c.counterparty_id 
+INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
+  Group by i.instrument_name, c.counterparty_name;  
 
 
