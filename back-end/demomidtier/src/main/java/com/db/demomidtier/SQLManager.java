bd35e663aff02fb80445b5ad3d20d795bdbf4ca3
Timur <timurbaev1996@yandex.ru>
2018-08-13 18:02:30 +0100

    Added autorization

--- back-end/demomidtier/src/main/java/com/db/demomidtier/SQLManager.java
+++ back-end/demomidtier/src/main/java/com/db/demomidtier/SQLManager.java
@@ -7,26 +7,35 @@ public class SQLManager {
     private DB db;
     public SQLManager (String dbName, String username, String password) {
         try {
-            db = new DB(dbName, username, password);     
+            db = new DB(dbName, username, password);
         } catch (Exception e) {
             throw new IllegalStateException("Cannot connect the database!", e); /// wrong
         }
     }
-    
-    public String testConnection () {
-        if (db == null) {
-            return "Failed to make connection!";
-        } else {
-            return "Successful";
-        }
-    }
-   public Map[] LoadUsers (String UserId) {
-        Map[] Users = null;
+
+    public Map LoadUserById (String UserId) throws Exception {
+        Map[] Users;
         try {
-            String query = "SELECT * FROM db_grad_cs_1917.users WHERE user_id = ?";
+            String query = "SELECT * FROM users WHERE user_id = ?";
             PreparedStatement preparedStatement = db.prepare(query);
             preparedStatement.setString (1, UserId);
             preparedStatement.execute();
+            ResultSet rs = preparedStatement.executeQuery();
+            Users = db.ResultSetToMapArray (rs);
+            if (Users.length == 1) {
+                return Users[0];
+            }
+        } catch (Exception e) {
+            System.err.println ("Got an exception!");
+            System.err.println (e.getMessage());
+        }
+        throw new Exception("");
+    }
+    public Map[] LoadUsers () {
+        Map[] Users = null;
+        try {
+            String query = "SELECT * FROM users";
+            PreparedStatement preparedStatement = db.prepare(query);
             ResultSet rs = preparedStatement.executeQuery();
             Users = db.ResultSetToMapArray (rs);
         } catch (Exception e) {
@@ -50,11 +59,10 @@ public class SQLManager {
         }
         return AnonymousUsers;
     }
-    public Map[] LoadCounterparties (){ //Integer CounterpartyId) {
+    public Map[] LoadCounterparties () {
         Map[] Counterparties = null;
         try {
-//            String query = "SELECT * FROM db_grad_cs_1917.counterparty WHERE counterparty_id = ?";
-            String query = "SELECT * FROM db_grad_cs_1917.counterparty";
+            String query = "SELECT * FROM counterparty";
             PreparedStatement preparedStatement = db.prepare(query);
           //  preparedStatement.setInt (1, CounterpartyId); /// Integer or Int
             preparedStatement.execute();
@@ -66,8 +74,29 @@ public class SQLManager {
         }
         return Counterparties;
     }
-    public boolean AuthorizeUser (String login, String password) {
-        
-        return false;
-    } 
+    public boolean AuthorizeUser (String UserId, String UserPwd) throws Exception {
+        try {
+            String query = "SELECT * FROM users WHERE user_id = ? AND user_pwd = ?";
+            PreparedStatement preparedStatement = db.prepare(query);
+            preparedStatement.setString (1, UserId);
+            preparedStatement.setString (2, UserPwd);
+            preparedStatement.execute();
+            ResultSet rs = preparedStatement.executeQuery();
+            Map[] Users = db.ResultSetToMapArray (rs);
+            switch (Users.length) {
+                case 0:
+                    throw new Exception("Auth Error");
+                case 1:
+                    return true; // Better Map
+                    //Auth Success
+                    //break;
+                default:
+                    throw new Exception("Database Error"); // Not unique key
+            }
+        } catch (Exception e) {
+            System.err.println ("Got an exception!");
+            System.err.println (e.getMessage());
+        }
+        throw new Exception("");
+    }
 }
