8194f6f15a40c92a29048f998f6d26313e282471
andreastichy91 <andreas.tichy@icloud.com>
2018-08-16 10:36:43 +0100

    Inserted final requirement

--- back-end/demomidtier/scripts/new_requ_sql_v2.sql
+++ back-end/demomidtier/scripts/new_requ_sql_v2.sql
@@ -1,10 +1,14 @@
-/* USER REQUIREMENT 3 */
+/* USER REQUIREMENT 3 
+FUNCTION : Overview
+*/
 SELECT d.deal_time, d.deal_type, d.deal_amount, d.deal_quantity, i.instrument_name, c.counterparty_name, c.counterparty_status, c.counterparty_date_registered
 FROM deal d, counterparty c, instrument i
 where d.deal_instrument_id = i.instrument_id and d.deal_counterparty_id = c.counterparty_id
 order by d.deal_id;
 
-/* USER REQUIREMENT 4 */
+/* USER REQUIREMENT 4 
+FUNCTION: Correalation
+*/
 
 /* Correlation btw. deal_type and counterparty --> SHows the most frequently traded Counterparty */
 SELECT sum(d.deal_quantity), sum(d.deal_amount), c.counterparty_name
@@ -23,6 +27,7 @@ SELECT sum(d.deal_quantity), sum(d.deal_amount), i.instrument_name
 
 /* Requirement 1 
 What are the average buy and sell prices for each instrument during the period? - importance (3), accuracy (4)
+FUNCTION: Average_price
 */
 SELECT i.instrument_name, AVG(d.deal_amount * d.deal_quantity) AS avg_sell_price
   FROM deal d, instrument i
@@ -38,6 +43,7 @@ SELECT i.instrument_name, AVG(d.deal_amount * d.deal_quantity) AS avg_sell_price
   
 /* Requirement 2
 What are the dealers ending positions (i.e. how many net trades have they done on each instrument)? - importance (4), accuracy (5)
+FUNCTION: End_Position
 */
 
 select  c.counterparty_name, i.instrument_name, SUM(case when d.deal_type = 'S' then d.deal_quantity else 0 end) - SUM(case when d.deal_type = 'B' then d.deal_quantity else 0 end) as net_trade
@@ -49,6 +55,7 @@ INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
 
 /* Requirement 3
 What is the realised profit/loss for each dealer (i.e. net sum of their trade values)? - importance (3), accuracy (5)
+FUNCTION: Realised_Profit
 */
 
 select  c.counterparty_name, i.instrument_name, 
@@ -67,10 +74,7 @@ INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id
 What is the effective profit/loss for each dealer (i.e. resolving their positions using the ending buy/sell price)? - importance (4), accuracy (5)
 ??????????????????????
 */
-
-/* 
-effective = realised - ending position * ending price per instrument
-*/
+select  i.instrument_name, (SUM(case when d.deal_type = 'S' then d.deal_amount * d.deal_quantity else 0 end) - SUM(case when d.deal_type = 'B' then d.deal_amount *d.deal_quantity else 0 end)) + (SUM(case when d.deal_type = 'S' then d.deal_quantity else 0 end) - SUM(case when d.deal_type = 'B' then d.deal_quantity else 0 end)) * (CASE WHEN i.instrument_name = 'Astronomica' THEN 2657.01 WHEN i.instrument_name = 'Borealis' THEN 6090.83 WHEN i.instrument_name = 'Celestial' THEN 1099.1 WHEN i.instrument_name = 'Deuteronic' THEN 3131.3 WHEN i.instrument_name = 'Eclipse' THEN 8913.57 WHEN i.instrument_name = 'Floral' THEN 507.72 WHEN i.instrument_name = 'Galactia' THEN 6574.05 WHEN i.instrument_name = 'Heliosphere' THEN 9025.46 WHEN i.instrument_name = 'Interstella' THEN 2485.94 WHEN i.instrument_name = 'Jupiter' THEN 4613.63 WHEN i.instrument_name = 'Koronis' THEN 2314.43 WHEN i.instrument_name = 'Lunatic' THEN 1235.49 ELSE 1000 END) as effective_result FROM  deal d INNER JOIN instrument i on  d.deal_instrument_id = i.instrument_id Group by i.instrument_name;  
 
 
 
